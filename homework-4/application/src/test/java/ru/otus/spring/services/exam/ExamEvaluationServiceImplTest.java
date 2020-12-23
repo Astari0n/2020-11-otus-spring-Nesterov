790edc70cf6d2d151f805b920955f93b5da9f16c
Astari0n <31375539+astari0n@users.noreply.github.com>
2020-12-23 18:43:24 +0300

    homework-4 pull request fix 2

--- homework-4/application/src/test/java/ru/otus/spring/services/exam/ExamEvaluationServiceImplTest.java
+++ homework-4/application/src/test/java/ru/otus/spring/services/exam/ExamEvaluationServiceImplTest.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import ru.otus.spring.exceptions.ExamEvaluationException;
 import ru.otus.spring.model.ExamAnswerForm;
@@ -30,18 +28,10 @@ import static org.mockito.BDDMockito.anyString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
+@SpringBootTest(classes = ExamEvaluationServiceImpl.class)
 @DisplayName("Класс ExamEvaluationServiceImpl")
 @SuppressWarnings("ALL")
 public class ExamEvaluationServiceImplTest {
-
-    @Configuration
-    static class ExamEvaluationServiceImplTestConfig {
-        @Bean
-        public ExamEvaluationServiceImpl examEvaluationServiceImpl(final MessageLocalizationService localizationService) {
-            return new ExamEvaluationServiceImpl(localizationService);
-        }
-    }
 
     @Autowired
     private ExamEvaluationServiceImpl service;
